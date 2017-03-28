980d839ec82d4c5606a1281572fd3f86526a6e8b
tosite0345 <tl091264@gmail.com>
2017-03-28 17:01:05 +0900

    履歴画面作成

---
+++ user/history/index.php
@@ -0,0 +1,69 @@
+<?php
+    require_once('../../public/assets/php/lib/user/userProcess.php');
+    require_once('../../public/assets/php/connectDb.php');
+    require_once('../../public/assets/php/getFontColor.php');
+    $lists = displayHistory(1, 1);
+    $total = 0;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CoopSystem</title>
+    <?php include("../../public/assets/php/partial/head.php"); ?>
+</head>
+<body>
+<?php include("../../public/assets/php/partial/header.php"); ?>
+
+<button class="col-btn" col-target="#col-menu"></button>
+
+<div class="flex">
+    <div class="col-3 border-right min-height" id="col-menu">
+        <?php include("../../public/assets/php/partial/menu_user.php"); ?>
+    </div>
+    <div class="col-9 container">
+        <h1>商品注文履歴確認画面</h1>
+        <table class="table-stripe border-bottom">
+            <thead>
+                <tr>
+                    <th class="text-center">カテゴリ</th>
+                    <th class="text-left">商品名</th>
+                    <th class="text-center">単価</th>
+                    <th class="text-center">購入数</th>
+                    <th class="text-right">合計金額</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($lists as $list){ ?>
+                <tr>
+                    <td class="text-center">
+                        <p class="label" style="background: <?php echo $list['color'] ?>; color: <?php echo getFontColor($list['color']) ?>;">
+                            <?php echo $list['category_name'] ?>
+                        </p>
+                    </td>
+                    <td class="text-left">
+                        <p><?php echo $list['goods_name'] ?></p>
+                    </td>
+                    <td class="text-center">
+                        <p><?php echo number_format(intval($list['unit_price'])) ?>円</p>
+                    </td>
+
+                    <td class="text-center">
+                        <p><?php echo $list['ordering_quantity'] ?>個</p>
+                    </td>
+                    <td class="text-right">
+                        <p><?php echo number_format(intval($list['amount'])) ?>円</p>
+                        <?php $total += intval($list['amount']) ?>
+                    </td>
+                </tr>
+                <?php } ?>
+                <tr>
+                    <td colspan="5" class="text-right"><?php echo number_format($total) ?>円</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php include("../../public/assets/php/partial/footer.php"); ?>
+</body>
+</html>

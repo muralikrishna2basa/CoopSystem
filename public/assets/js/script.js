a76d0035c587458478d8a4f575ea8f749a72d6a3
tosite0345 <tl091264@gmail.com>
2017-03-28 15:31:35 +0900

    メニュー画面作成　デザイン微調整

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,10 @@
 $(function(){
+    //header-footer
+    var top = $("header").outerHeight();
+    var btm = $("footer").outerHeight();
+//    console.log(top+' ; '+btm);
+    $("body").css({paddingTop: top+'px', paddingBottom: btm+'px'});
+
     //Draggable
     $(".draggable").draggable({ containment: $(this).attr("data-area") });
 
@@ -15,7 +21,44 @@ $(function(){
 
     //Modal
     $(".modal-btn").click(function(){
-        console.log($(this).attr("modal-target"));
+        var src = $(this).attr("modal-target");
         $("body").append('<div class="modal-bg"></div>');
+        $(".modal-bg").hide().fadeIn(function(){
+            var html =
+                '<div class="modal-content" style="top: '+($(window).scrollTop()+150)+'px">'+
+                '   <span class="modal-close">&times;</span>'+
+                '   '+$(src).html()+
+                '</div>';
+            $(this).append(html);
+        });
+    });
+    $(document).on('click', '.modal-close', function(){ $(".modal-bg").fadeOut(function(){ $(this).remove() }) });
+
+    //Tips
+    $(".tips-trigger").hover(
+        function(){
+            var html = $(this).nextAll('.tips-target').html();
+            $(this).parent().append('<div class="tips-content">'+html+'</div>');
+            $(".tips-content").css({left: $(this).scrollLeft()+20+"px"}).hide().fadeIn();
+        }, function(){
+            $(".tips-content").fadeOut(100, function(){ $(this).remove() });
+        }
+    );
+
+    //col-btn
+    var trg  = $(".col-btn").attr("col-target");
+    var btm  = $(window).height();
+    var side = $(trg).innerWidth();
+//    console.log(trg+btm+' ; '+side);
+    $(".col-btn").css({top: (btm-60)+"px", left: "0px"});
+    $(".col-btn").click(function(){
+        var trg = $(this).attr("col-target");
+        if($(trg).is(":hidden")){
+            $(trg).fadeIn();
+            $(this).removeClass('col-close');
+        }else{
+            $(trg).fadeOut();
+            $(this).addClass('col-close');
+        }
     });
 })

aaa0d6e6fde989bc5a7687ff36f53171cb3afe01
tosite0345 <tl091264@gmail.com>
2017-03-28 17:00:13 +0900

    色による文字色判定関数作成

---
+++ public/assets/php/getFontColor.php
@@ -0,0 +1,12 @@
+<?php
+    function getFontColor($rgb = "#999999")
+    {
+        $rgb  = mb_substr($rgb, 1, 9);
+        $r    = hexdec( mb_substr( $rgb, 0, 2 ) );
+        $g    = hexdec( mb_substr( $rgb, 2, 2 ) );
+        $b    = hexdec( mb_substr( $rgb, 4, 2 ) );
+        $stat = ( $r*0.299 + $g*0.587 + $b*0.114 ) / 2.55;
+        if($stat > 50) return "#222222";
+        return "#ffffff";
+    }
+?>

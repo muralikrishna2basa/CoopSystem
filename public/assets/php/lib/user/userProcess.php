a0a40254ba6e82a62444690817eb8c212861f67c
tosite0345 <tl091264@gmail.com>
2017-03-28 11:04:24 +0900

    SQL文修正　インデント調整

--- public/assets/php/lib/user/userProcess.php
+++ public/assets/php/lib/user/userProcess.php
@@ -1,36 +1,34 @@
-<?php 
+<?php
 require_once('../../php/connectDb.php');
 
     //ユーザーの履歴を返す関数
     // カテゴリ名（category_name）,色（color）,商品名（goods_name）,単価（unit_price,）
     //発注数（ordering_quantity）,合計金額（(amount）の配列を返す
 function displayHistory($userId,$monthlyId){
-    
+
     $ren = [];
     try{
         $pdo = connectDb('cooopshinren');
-        if($monthlyId==0){     
-            $sql = "select category_name,color,goods_name,unit_price,
-        ordering_quantity,(unit_price*ordering_quantity)as amount
-        from  ordering_list
-        INNER JOIN monthly_goods on ordering_list.monthly_goods_id = monthly_goods.monthly_goods_id
-        INNER JOIN ordering on ordering_list.ordering_id = ordering.ordering_id
-        INNER JOIN category on monthly_goods.category_id = category.category_id
-        INNER JOIN monthly on ordering.monthly_id =monthly.monthly_id
-        WHERE ordering.orderer = ?";
+        if($monthlyId==0){
+            $sql = "SELECT category_name,color,goods_name,unit_price,
+            ordering_quantity,(unit_price*ordering_quantity) AS amount
+            FROM  ordering_list
+            INNER JOIN monthly_goods ON ordering_list.monthly_goods_id = monthly_goods.monthly_goods_id
+            INNER JOIN ordering ON ordering_list.ordering_id = ordering.ordering_id
+            INNER JOIN category ON monthly_goods.category_id = category.category_id
+            INNER JOIN monthly ON ordering.monthly_id =monthly.monthly_id
+            WHERE ordering.orderer = ?;";
             $stmt=$pdo->prepare($sql);
             $res= $stmt->execute(array($userId));
-           
         }else{
-            
-            $sql = "select category_name,color,goods_name,unit_price,
-        ordering_quantity,(unit_price*ordering_quantity)as amount
-        from  ordering_list
-        INNER JOIN monthly_goods on ordering_list.monthly_goods_id = monthly_goods.monthly_goods_id
-        INNER JOIN ordering on ordering_list.ordering_id = ordering.ordering_id
-        INNER JOIN category on monthly_goods.category_id = category.category_id
-        INNER JOIN monthly on ordering.monthly_id =monthly.monthly_id
-        WHERE ordering.orderer = ? and ordering.monthly_id=?";
+            $sql = "SELECT category_name,color,goods_name,unit_price,
+            ordering_quantity,(unit_price*ordering_quantity)as amount
+            FROM  ordering_list
+            INNER JOIN monthly_goods ON ordering_list.monthly_goods_id = monthly_goods.monthly_goods_id
+            INNER JOIN ordering ON ordering_list.ordering_id = ordering.ordering_id
+            INNER JOIN category ON monthly_goods.category_id = category.category_id
+            INNER JOIN monthly ON ordering.monthly_id =monthly.monthly_id
+            WHERE ordering.orderer = ? AND ordering.monthly_id=?;";
             $stmt=$pdo->prepare($sql);
             $res= $stmt->execute(array($userId,$monthlyId));
         }
@@ -49,15 +47,14 @@ function displayHistory($userId,$monthlyId){
 //一つ当たりの量（detail_amount_per_one）必要量（required_quantity),カテゴリ名（category_name）,色（color）
 //の配列を返す
 function returnCurrentMonthProductList(){
-   
 //    $ren=array();
     $ren = [];
     try{
         $pdo = connectDb('cooopshinren');
-        $sql="select monthly_goods_id,goods_name,unit_price,detail_amount_per_one, required_quantity,category_name,color from monthly_goods natural join monthly natural join category where public_flag=0";
+        $sql="SELECT monthly_goods_id,goods_name,unit_price,detail_amount_per_one, required_quantity,category_name,color
+        FROM monthly_goods NATURAL JOIN monthly NATURAL JOIN category WHERE public_flag=0;";
         $stmt=$pdo->prepare($sql);
         $res= $stmt->execute(null);
-        
 //        foreach ($stmt as $key => $value) {
 //            var_dump($value);
 //        }
@@ -78,31 +75,30 @@ function returnCurrentMonthProductList(){
 //一つ当たりの量(detail_amount_per_one）,在庫量（stock_quantity）,カテゴリ名（category_name）,色（color）の配列を返す
 
 function returnStockList($monthlyId){
-    
     $ren = [];
     try{
         $pdo = connectDb('cooopshinren');
         if ($monthlyId==0){
-            $sql = "select stock_list.monthly_goods_id,goods_name,unit_price,detail_amount_per_one,
-            stock_quantity,category_name,color 
-            from stock_list
-            INNER JOIN monthly_goods on stock_list.monthly_goods_id = monthly_goods.monthly_goods_id 
-            INNER JOIN category on monthly_goods.category_id = category.category_id";
+            $sql = "SELECT stock_list.monthly_goods_id,goods_name,unit_price,detail_amount_per_one,
+            stock_quantity,category_name,color
+            FROM stock_list
+            INNER JOIN monthly_goods ON stock_list.monthly_goods_id = monthly_goods.monthly_goods_id
+            INNER JOIN category ON monthly_goods.category_id = category.category_id;";
             $stmt=$pdo->prepare($sql);
             $res= $stmt->execute(null);
         }else{
-            $sql = "select stock_list.monthly_goods_id,goods_name,unit_price,detail_amount_per_one,
-            stock_quantity,category_name,color 
-            from stock_list
-            INNER JOIN monthly_goods on stock_list.monthly_goods_id = monthly_goods.monthly_goods_id 
-            INNER JOIN category on monthly_goods.category_id = category.category_id WHERE stock_list.monthly_id =?";
+            $sql = "SELECT stock_list.monthly_goods_id,goods_name,unit_price,detail_amount_per_one,
+            stock_quantity,category_name,color
+            FROM stock_list
+            INNER JOIN monthly_goods ON stock_list.monthly_goods_id = monthly_goods.monthly_goods_id
+            INNER JOIN category ON monthly_goods.category_id = category.category_id WHERE stock_list.monthly_id =?;";
             $stmt=$pdo->prepare($sql);
             $res= $stmt->execute(array($monthlyId));
         }
 
         while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-            $ren[] =$row;   
-        }     
+            $ren[] =$row;
+        }
         return $ren;
     }catch(Exception $e){
         echo $e->getMessage;
@@ -112,21 +108,19 @@ function returnStockList($monthlyId){
     //受け取ったデータを基に発注を行う関数
     //ユーザーIDと配列（商品ＩＤと個数）を受け取り登録する
 function doOrder($userId,$orderGoodsList){
-    
     try{
-     $pdo = connectDb('cooopshinren');
-     $sql="SELECT max(ordering_id) FROM ordering WHERE orderer = ?";
-     $stmt=$pdo->prepare($sql);
-     $res= $stmt->execute(array($userId));
-     while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-        $ren =$row[0];   
-    }    
-
+       $pdo = connectDb('cooopshinren');
+       $sql="SELECT MAX(ordering_id) FROM ordering WHERE orderer = ?";
+       $stmt=$pdo->prepare($sql);
+       $res= $stmt->execute(array($userId));
+       while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+        $ren =$row[0];
+    }
     foreach ($orderGoodsList as $key1 => $value1) {
 
         $pdo = connectDb('cooopshinren');
-        $sql ="INSERT INTO ordering_list VALUES (NULL,?,?,?)";
-        $stmt=$pdo->prepare($sql); 
+        $sql ="INSERT INTO ordering_list VALUES (NULL,?,?,?);";
+        $stmt=$pdo->prepare($sql);
         $res= $stmt->execute(array($key1,$ren,$value1));
     }
     return 5;
@@ -137,23 +131,23 @@ function doOrder($userId,$orderGoodsList){
 }
 function fixedFlagChange($userId,$orderGoodsList){
     try{
-         $pdo = connectDb('cooopshinren');
-         $sql="SELECT max(ordering_id) FROM ordering WHERE orderer = ?";
-     $stmt=$pdo->prepare($sql);
-     $res= $stmt->execute(array($userId));
-     while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-        $ren =$row[0];   
-    }    
-     $pdo = connectDb('cooopshinren');
-     $sql = "UPDATE ordering SET fixed_flag = 0 WHERE ordering.orderer =?";
-     $stmt=$pdo->prepare($sql);
-     $res= $stmt->execute(array($userId));
-$pdo = connectDb('cooopshinren');
-     $sql = "UPDATE ordering SET fixed_flag = 1 WHERE ordering.ordering_id=?";
-     $stmt=$pdo->prepare($sql);
-     $res= $stmt->execute(array($ren));
-    }catch(Exception $e){
-        echo $e->getMessage;
+       $pdo = connectDb('cooopshinren');
+       $sql="SELECT MAX(ordering_id) FROM ordering WHERE orderer = ?;";
+       $stmt=$pdo->prepare($sql);
+       $res= $stmt->execute(array($userId));
+       while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
+        $ren =$row[0];
     }
+    $pdo = connectDb('cooopshinren');
+    $sql = "UPDATE ordering SET fixed_flag = 0 WHERE ordering.orderer =?;";
+    $stmt=$pdo->prepare($sql);
+    $res= $stmt->execute(array($userId));
+    $pdo = connectDb('cooopshinren');
+    $sql = "UPDATE ordering SET fixed_flag = 1 WHERE ordering.ordering_id=?;";
+    $stmt=$pdo->prepare($sql);
+    $res= $stmt->execute(array($ren));
+}catch(Exception $e){
+    echo $e->getMessage;
+}
 
 }

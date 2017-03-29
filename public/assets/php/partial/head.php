4f2748de600c8562dfac7bd4614fe487ad391942
tosite0345 <tl091264@gmail.com>
2017-03-29 09:24:07 +0900

    favicon変更

--- public/assets/php/partial/head.php
+++ public/assets/php/partial/head.php
@@ -1,4 +1,8 @@
-<?php $path = "http://localhost/sample/CoopSystem"; ?>
+<?php $path = "http://localhost/sys/sam/CoopSystem"; ?>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="icon" href="<?php echo $path ?>/public/assets/favicon.ico">
 <link href="<?php echo $path ?>/public/assets/stylesheets/style.css" rel="stylesheet" />
 <link href="<?php echo $path ?>/public/assets/stylesheets/scripts.css" rel="stylesheet" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>

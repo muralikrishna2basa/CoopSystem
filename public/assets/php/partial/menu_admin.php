d1587cd50359fb81a42309e33ccdf77ef29f3b54
tosite0345 <tl091264@gmail.com>
2017-03-31 14:45:59 +0900

    メニュー回り変更

--- public/assets/php/partial/menu_admin.php
+++ public/assets/php/partial/menu_admin.php
@@ -3,11 +3,10 @@
     <h2 class="text-center">管理者メニュー</h2>
     <nav>
         <ul>
-            <li><a href="<?php echo $url ?>/admin/month">月ごとの編集を行う</a></li>
-            <li><a href="<?php echo $url ?>/user/stock">在庫を登録する</a></li>
-            <li><a href="<?php echo $url ?>/user/history">在庫を編集する</a></li>
-            <li><a href="<?php echo $url ?>/user/stock">現在のリストを編集する</a></li>
-            <li><a href="<?php echo $url ?>/user/history">注文リストを修正する</a></li>
+            <li><a href="<?php echo $url ?>/admin/month">公開する月を選ぶ</a></li>
+            <li><a href="<?php echo $url ?>/admin/stock">在庫を登録する</a></li>
+            <li><a href="<?php echo $url ?>/admin/month">注文リストを修正する</a></li>
+            <li><a href="<?php echo $url ?>/admin/month">発注を確定する</a></li>
             <li><a href="<?php echo $url ?>/admin/category">カテゴリを編集する</a></li>
             <li><a href="<?php echo $url ?>/public/assets/php/partial/logout.php">ログアウトする</a></li>
         </ul>
--- public/assets/php/partial/menu_user.php
+++ public/assets/php/partial/menu_user.php
@@ -1,11 +1,12 @@
 <?php $url = 'http://localhost/sys/sam/CoopSystem' ?>
 <div class="menu">
-    <h2>Menu</h2>
+    <h2 class="text-center">メニュー</h2>
     <nav>
         <ul>
             <li><a href="<?php echo $url ?>/user/order">商品を注文する</a></li>
             <li><a href="<?php echo $url ?>/user/stock">在庫から注文する</a></li>
-            <li><a href="#">履歴を確認する</a></li>
+            <li><a href="<?php echo $url ?>/user/history">履歴を確認する</a></li>
+            <li><a href="<?php echo $url ?>/public/assets/php/partial/logout.php">ログアウトする</a></li>
         </ul>
     </nav>
 </div>

fba2e1cb8bd5b08bb46bb5f3d5ec033fef972194
tosite0345 <tl091264@gmail.com>
2017-04-05 10:56:02 +0900

    環境変数の調整

--- public/assets/php/partial/require_common.php
+++ public/assets/php/partial/require_common.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
-$URL  = 'http://localhost/sample/CoopSystem';
-$PATH = 'C:\xampp\htdocs\sample\CoopSystem';
+// $URL  = 'http://localhost/sample/CoopSystem';
+// $PATH = 'C:\xampp\htdocs\sample\CoopSystem';
+$URL  = 'http://localhost/sys/sam/CoopSystem';
+$PATH = 'C:\xampp\htdocs\sys\sam\CoopSystem';
 require_once($PATH.'/public/assets/php/lib/common/connectDb.php');
 require_once($PATH.'/public/assets/php/lib/common/getFontColor.php');
 require_once($PATH.'/public/assets/php/lib/common/errorMessages.php');
